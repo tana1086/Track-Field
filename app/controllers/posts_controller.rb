1f9bbacfd6907f4c9455d3eeda4cc90de11fc63e
tana1086 <1086tn2175@gmail.com>
2025-02-03 10:22:24 +0900

    testコード修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,10 +27,9 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        render new_post_path, status: :unprocessable_entity
       end
     end
   end
@@ -40,10 +39,8 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post, notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
     end
   end
@@ -54,7 +51,6 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to posts_path, status: :see_other, notice: "Post was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
