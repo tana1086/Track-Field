9c1599a7108c5bfe7ff3e392f65425fe52646b88
tana1086 <1086tn2175@gmail.com>
2025-02-03 13:42:21 +0900

    fix: タイポ修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "track-field.onrender.com", protocpl: "https" }
+  config.action_mailer.default_url_options = { host: "track-field.onrender.com", protocol: "https" }
   config.action_mailer.delivery_method =:smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
