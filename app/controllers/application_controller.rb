a9202d8c861857ce9ff1d78942041ec5052bdef1
tana1086 <1086tn2175@gmail.com>
2025-02-04 18:59:01 +0900

    Add: Profileの初期設定が済んでいない場合、newに遷移

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def after_sign_in_path_for(resource)
+    if current_user.profile&.name.nil?
+      new_profile_path
+    else
+      posts_path
+    end
+  end
 end
