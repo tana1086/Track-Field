312ef50f31c01140bb0a269cee5548f6b54bcc9d
tana1086 <1086tn2175@gmail.com>
2025-02-11 16:49:55 +0900

    add: enumの追加

--- app/models/competition_result.rb
+++ app/models/competition_result.rb
@@ -1,4 +1,6 @@
 class CompetitionResult < ApplicationRecord
     belongs_to :user
     belongs_to :sprint
+
+    enum event_type: { no_event: 0, sprint: 1, middle_and_long: 2, jump: 3, throwing: 4, combined: 5 }
 end
