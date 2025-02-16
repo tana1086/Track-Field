9504667e9a3c0c909320326a561ee22837b4fa98
tana1086 <1086tn2175@gmail.com>
2025-02-16 20:43:50 +0900

    add: modelにscopeの追加

--- app/models/competition_result.rb
+++ app/models/competition_result.rb
@@ -2,5 +2,7 @@ class CompetitionResult < ApplicationRecord
     belongs_to :user
     has_many :sprints, dependent: :destroy
 
+    scope :with_sprint, -> { joins(:sprints).select("competition_results.*, sprints.record, sprints.wind_speed")}
+
     enum event_type: { no_event: 0, sprint: 1, middle_and_long: 2, jump: 3, throwing: 4, combined: 5 }
 end
