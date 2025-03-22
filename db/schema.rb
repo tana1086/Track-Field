35ed553d61a2231744fb9007bdf8cfdc8af4404b
tana1086 <1086tn2175@gmail.com>
2025-03-22 12:51:03 +0900

    add: likeモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_19_061236) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_22_025839) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -72,6 +72,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_19_061236) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["middle_and_long_id"], name: "index_lap_times_on_middle_and_long_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "middle_and_longs", force: :cascade do |t|
@@ -145,6 +154,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_19_061236) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "competition_results", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
   add_foreign_key "profiles", "users"
   add_foreign_key "sprints", "competition_results"
