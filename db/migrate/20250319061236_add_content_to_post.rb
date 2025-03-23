5f81431c6d75b2740b389a118a496848fd7994b2
tana1086 <1086tn2175@gmail.com>
2025-03-23 14:36:58 +0900

    fix: postのデフォルト値設定

--- db/migrate/20250319061236_add_content_to_post.rb
+++ db/migrate/20250319061236_add_content_to_post.rb
@@ -1,5 +1,5 @@
 class AddContentToPost < ActiveRecord::Migration[7.2]
   def change
-    add_column :posts, :content, :text, null: false
+    add_column :posts, :content, :text, null: false, default: ""
   end
 end
